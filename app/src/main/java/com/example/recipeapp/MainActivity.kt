11e2d253e67e433e5af183b7d57b236810080d7e
toto-neutrino1 <tolyan7007@gmail.com>
2023-12-07 15:11:38 +0300

    RA03_navigation buttons layout creating

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.databinding.ActivityMainBinding
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalArgumentException("ActivityMainBinding is null!")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
