2bb210484f31f1cc51fa227d51b50d6ed29323b3
toto-neutrino1 <tolyan7007@gmail.com>
2023-12-26 02:59:49 +0300

    RA-07 reformat code, + object & line break in TextViews

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -12,46 +12,6 @@ class CategoriesListFragment : Fragment() {
     private val binding
         get() = _binding ?: throw IllegalArgumentException("CategoriesListFragmentBinding is null!")
 
-    val categoriesList =
-        listOf(
-            Category(
-                id = 0,
-                title = "Бургеры",
-                description = "Рецепты всех популярных видов бургеров",
-                imageUrl = "burger.png"
-            ),
-            Category(
-                id = 1,
-                title = "Десерты",
-                description = "Самые вкусные рецепты десертов специально для вас",
-                imageUrl = "dessert.png"
-            ),
-            Category(
-                id = 2,
-                title = "Пицца",
-                description = "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
-                imageUrl = "pizza.png"
-            ),
-            Category(
-                id = 3,
-                title = "Рыба",
-                description = "Печеная, жареная, сушеная, любая рыба на твой вкус",
-                imageUrl = "fish.png"
-            ),
-            Category(
-                id = 4,
-                title = "Супы",
-                description = "От классики до экзотики: мир в одной тарелке",
-                imageUrl = "soup.png"
-            ),
-            Category(
-                id = 5,
-                title = "Салаты",
-                description = "Хрустящий калейдоскоп под соусом вдохновения",
-                imageUrl = "salad.png"
-            )
-        )
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
