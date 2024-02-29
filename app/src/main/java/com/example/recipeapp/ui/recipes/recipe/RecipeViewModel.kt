23f72ca6cd1e9c2fa0d43ddd77c51a87b7041f9d
toto-neutrino1 <tolyan7007@gmail.com>
2024-02-29 16:17:50 +0300

    RA-21 create livedata to store screen state

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.recipeapp.ui.recipes.recipe
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recipeapp.model.Recipe
 
@@ -9,4 +11,8 @@ data class RecipeUiState(
     var isInFavorites: Boolean = false
 )
 
-class RecipeViewModel : ViewModel() {}
+class RecipeViewModel : ViewModel() {
+    private var _recipeUiState: MutableLiveData<RecipeUiState> = MutableLiveData()
+    val recipeUiState: LiveData<RecipeUiState>
+        get() = _recipeUiState
+}
