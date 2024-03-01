030d743227b29b9f4181f9e08ccff875276ad47d
toto-neutrino1 <tolyan7007@gmail.com>
2024-03-02 01:01:01 +0300

    RA-22 subscribe to livedata changes from fragment

--- app/src/main/java/com/example/recipeapp/data/Constants.kt
+++ app/src/main/java/com/example/recipeapp/data/Constants.kt
@@ -9,3 +9,4 @@ const val ARG_RECIPE = "ARG_RECIPE"
 const val NUM_OF_INGREDIENT_MANTIS = 1
 const val SHARED_FAVORITES_IDS_FILE_NAME = "com.example.recipeapp.sharedFavoritesIds"
 const val SHARED_FAVORITES_IDS_KEY = "sharedFavoritesIds"
+const val TAG_RECIPE_VIEW_MODEL = "RecipeViewModel"
--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.recipeapp.ui.recipes.recipe
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.recipeapp.data.TAG_RECIPE_VIEW_MODEL
 import com.example.recipeapp.model.Recipe
 
 data class RecipeUiState(
@@ -13,6 +15,10 @@ data class RecipeUiState(
 
 class RecipeViewModel : ViewModel() {
     private var _recipeUiState: MutableLiveData<RecipeUiState> = MutableLiveData()
-    val recipeUiState: LiveData<RecipeUiState>
-        get() = _recipeUiState
+    val recipeUiState: LiveData<RecipeUiState> = _recipeUiState
+
+    init {
+        Log.i(TAG_RECIPE_VIEW_MODEL, "RecipeViewModel init block")
+        _recipeUiState.value = RecipeUiState(isInFavorites = true)
+    }
 }
