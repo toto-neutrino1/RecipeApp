53ae70c3610cd3107fba98c9fa3bc8bcadabc43e
toto-neutrino1 <tolyan7007@gmail.com>
2024-03-30 16:43:57 +0300

    RA-33 create thread pool

--- app/src/main/java/com/example/recipeapp/data/Constants.kt
+++ app/src/main/java/com/example/recipeapp/data/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.recipeapp.data
 
 const val URL_GET_CATEGORIES = "https://recipes.androidsprint.ru/api/category"
+const val URL_GET_RECIPES_SUFFIX = "recipes"
 
 const val NUM_OF_INGREDIENT_MANTIS = 1
 const val SHARED_FAVORITES_IDS_FILE_NAME = "com.example.recipeapp.sharedFavoritesIds"
